49ffd411bd029a9d9809647b3915a4c1243e241e
Cassandra <45634386+CassandraCH@users.noreply.github.com>
2021-03-01 12:13:17 +0100

    Nettoyage des fichiers inutiles

---
+++ src/app/sections/recette/recette.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recette',
+  templateUrl: './recette.component.html',
+  styleUrls: ['./recette.component.css']
+})
+export class RecetteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
