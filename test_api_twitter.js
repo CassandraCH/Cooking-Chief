a39dd442ddf2f508faeb300dea6164c2bf3d2c6e
Darskel <60432979+Darskel@users.noreply.github.com>
2021-03-09 11:57:13 +0100

    test api twitter

---
+++ test_api_twitter.js
@@ -0,0 +1,15 @@
+var Twitter = require('twitter');
+
+var client = new Twitter({
+    consumer_key: 'EuKQfwTijUYZBkSxmgqoPir6F',
+    consumer_secret: 'xVYQgEZN4L1VYBszUStZEiH6VhjJbMyeQyAzMYB0TwR0bDU9Sg',
+    access_token_key: '1359508421301727232-DUalWqDuPUC21F9xd9AZ8LKrQgMVW8',
+    access_token_secret: 'k7Z60xBmdUFFiLvQkM3yU9KayhvCTQhwvd2pVjho9Ll5s'
+  });
+
+var params = {screen_name: 'web_l3'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    console.log(tweets);
+  }
+});
