22c1170a4ce3009c9eca572feeb57219c5235880
Guillaume <laukagui@gmail.com>
2021-03-13 19:45:52 +0100

    Correction erreur

--- src/assets/js/test_api_edamam.js
+++ src/assets/js/test_api_edamam.js
@@ -7,7 +7,7 @@ searchButton.addEventListener("click", ()=>{
 
 async function requeteApi(){
     let APP_ID = "4c9755a7";
-    let APP_KEY = "4f882faf5f91c89ff3bede0a2e4961d6";
+    let APP_KEY = "4f1e59bb1fddf09974a0bb3b33de90d8";
 
     let reponse = await fetch('https://api.edamam.com/search?app_id=${APP_ID}&app_key=${APP_KEY}&q=pizza');
     console.log(response);
