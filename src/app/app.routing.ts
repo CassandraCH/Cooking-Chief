4d394c5103f7c8e0cb966891e4aac215179f7fdc
Cassandra <45634386+CassandraCH@users.noreply.github.com>
2021-03-13 21:34:34 +0100

    Modification css header + footer

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -18,9 +18,10 @@ const routes: Routes = [
   imports: [
     CommonModule,
     BrowserModule,
-    RouterModule.forRoot(routes,{
-      useHash: true
-    })
+    RouterModule.forRoot(routes)
+    // RouterModule.forRoot(routes,{
+    //   useHash: true
+    // })
   ],
   exports: [
   ],
