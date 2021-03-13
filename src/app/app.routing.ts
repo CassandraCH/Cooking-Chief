80e29e4469c75354bdbeea5636edb3a9b9c3f969
Cassandra <45634386+CassandraCH@users.noreply.github.com>
2021-03-13 20:36:55 +0100

    Ajout de la page 404 dans le routing (gestion des erreurs de saisie dans l'url)

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,12 +4,14 @@ import { BrowserModule  } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from '../app/components/home/home.component';
+import { ErreurPageComponent } from './components/erreur-page/erreur-page.component';
 
 // Gestion du routing
 const routes: Routes = [
-    { path: 'home',             component: HomeComponent },
-    { path: '', redirectTo: 'home', pathMatch: 'full' },
-    { path: '**', redirectTo: 'home'}
+    { path: 'home', component: HomeComponent },
+    { path: '', redirectTo: 'home', pathMatch: 'full' }, // par défaut
+    { path: 'not-found', component: ErreurPageComponent },
+    { path: '**', redirectTo: '/not-found'} // gestion des erreurs de saisie de l'url => page 404
 ];
 
 @NgModule({
