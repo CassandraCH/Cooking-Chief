51a7a0a88568dff24df4a95ff39f7923f4aa936b
Darskel <60432979+Darskel@users.noreply.github.com>
2021-03-15 21:15:52 +0100

    changement test twitter

--- test_api/test_api_twitter.js
+++ test_api/test_api_twitter.js
@@ -1,15 +1,16 @@
-var Twitter = require('twitter');
+var Twitter = require('twit');
 
 var client = new Twitter({
     consumer_key: 'EuKQfwTijUYZBkSxmgqoPir6F',
     consumer_secret: 'xVYQgEZN4L1VYBszUStZEiH6VhjJbMyeQyAzMYB0TwR0bDU9Sg',
-    access_token_key: '1359508421301727232-DUalWqDuPUC21F9xd9AZ8LKrQgMVW8',
+    access_token: '1359508421301727232-DUalWqDuPUC21F9xd9AZ8LKrQgMVW8',
     access_token_secret: 'k7Z60xBmdUFFiLvQkM3yU9KayhvCTQhwvd2pVjho9Ll5s'
   });
 
-var params = {screen_name: 'web_l3'};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-    console.log(tweets);
-  }
-});
+var params = { q: '#cooking_chief'};
+client.get('search/tweets', params, function(error, data, response) {
+  const tweets = data.statuses
+  .map(tweet => tweet.text);
+
+  console.log(tweets);
+  });
